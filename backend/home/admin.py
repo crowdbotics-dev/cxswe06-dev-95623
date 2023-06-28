fac4a8f4ac21642b5c6f7e387beed82e6665b220
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-28 17:24:18 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Zaqw
+admin.site.register(Zaqw)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Zaqw(models.Model):
+    'Generated Model'
+    mkj = models.BigIntegerField()
